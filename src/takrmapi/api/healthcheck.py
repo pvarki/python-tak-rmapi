a2ed8550613963c177fd92b6e62d80d918d1b9ae
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-11-04 03:50:37 +0200

    serve healthcheck correctly

--- src/takrmapi/api/healthcheck.py
+++ src/takrmapi/api/healthcheck.py
@@ -1,9 +1,11 @@
 """Endpoints for information for the admin"""
+from typing import Optional
 import logging
 import os
-from typing import Dict, Any
-from fastapi import APIRouter, Depends, HTTPException, Request
+
+from fastapi import APIRouter, Depends, Request
 from libpvarki.middleware import MTLSHeader
+from libpvarki.schemas.product import ProductHealthCheckResponse
 
 LOGGER = logging.getLogger(__name__)
 
@@ -12,28 +14,21 @@ router = APIRouter()
 
 
 @router.get("")
-async def get_healthcheck() -> Dict[Any, Any]:
-    """Basic healthcheck"""
-    returnable: Dict[Any, Any] = {"healthcheck": "success"}
-    return returnable
-
-
-@router.get("/delivery_status")
 async def get_healthcheck_delivery_status(
     request: Request,
-) -> Dict[Any, Any]:
+) -> ProductHealthCheckResponse:
     """Return 200 when api is ready to deliver"""
     ready_to_serve: bool = False
+    reason: Optional[str] = None
 
     # TODO do some actual logic to check if we have something to serve...
+    # FIXME: If using this, at least use pathlib...
     dir_files = os.listdir("/opt/tak/data/certs/files")
     if len(dir_files) > 0:
         ready_to_serve = True
 
     if not ready_to_serve:
-        _reason = "Waiting for RM to give go ahead."
-        LOGGER.info("{} : {}".format(request.url, _reason))
-        raise HTTPException(status_code=420, detail=_reason)
+        reason = "Waiting for RM to give go ahead."
+        LOGGER.info("{} : {}".format(request.url, reason))
 
-    returnable: Dict[Any, Any] = {"status": "rdy-to-serve"}
-    return returnable
+    return ProductHealthCheckResponse(healthy=ready_to_serve, extra=reason)
