1c8015eb7f8be78b495f89a6cd61c20ac0e2b491
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-01-29 21:33:23 +0200

    add healthcheck CLI command

--- src/takrmapi/console.py
+++ src/takrmapi/console.py
@@ -1,9 +1,11 @@
 """CLI entrypoints for product integration api"""
+import asyncio
 import logging
 import json
 
 import click
 from libadvian.logging import init_logging
+import aiohttp
 
 from takrmapi import __version__
 from takrmapi.app import get_app
@@ -26,6 +28,34 @@ def cli_group(ctx: click.Context, loglevel: int, verbose: int) -> None:
 
     LOGGER.setLevel(loglevel)
     ctx.ensure_object(dict)
+
+
+@cli_group.command(name="healthcheck")
+@click.option("--host", default="localhost", help="The host to connect to")
+@click.option("--port", default=8003, help="The port to connect to")
+@click.option("--timeout", default=2.0, help="The timeout in seconds")
+@click.pass_context
+def do_http_healthcheck(ctx: click.Context, host: str, port: int, timeout: float) -> None:
+    """
+    Do a GET request to the healthcheck api and dump results to stdout
+    """
+
+    async def doit() -> int:
+        """The actual work"""
+        nonlocal host, port, timeout
+        if "://" not in host:
+            host = f"http://{host}"
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=timeout)) as session:
+            async with session.get(f"{host}:{port}/api/v1/healthcheck") as resp:
+                if resp.status != 200:
+                    return resp.status
+                payload = await resp.json()
+                click.echo(json.dumps(payload))
+                if not payload["healthy"]:
+                    return 1
+        return 0
+
+    ctx.exit(asyncio.get_event_loop().run_until_complete(doit()))
 
 
 @cli_group.command(name="openapi")
