e9e2c85250501a92614f48d9b87841d7efcdc8f4
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-07-06 19:50:37 +0300

    deprecate old "instructions" endpoints, add product description endpoint

--- src/takrmapi/api/__init__.py
+++ src/takrmapi/api/__init__.py
@@ -6,6 +6,7 @@ from .usercrud import test_router as testing_router  # REMOVE ME
 from .clientinfo import router as clientinfo_router
 from .admininfo import router as admininfo_router
 from .healthcheck import router as healthcheck_router
+from .description import router as description_router
 
 all_routers = APIRouter()
 all_routers.include_router(testing_router, prefix="/users", tags=["users"])  # REMOVE ME
@@ -13,3 +14,4 @@ all_routers.include_router(usercrud_router, prefix="/users", tags=["users"])
 all_routers.include_router(clientinfo_router, prefix="/clients", tags=["clients"])
 all_routers.include_router(admininfo_router, prefix="/admins", tags=["admins"])
 all_routers.include_router(healthcheck_router, prefix="/healthcheck", tags=["healthcheck"])
+all_routers.include_router(description_router, prefix="/description", tags=["description"])
