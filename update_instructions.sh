fdaad7b0524b0d9952ad44a0ceec81f0877162a8
Ari Karhunen <ari.karhunen@remod.fi>
2024-12-15 15:39:01 +0200

    comments to udpate_instructions.sh

--- update_instructions.sh
+++ update_instructions.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Dockerfile takes the generated tak.json and stores it under /opt/templates/tak.json where the
+# api serves it.
+
+# https://github.com/hyperifyio/rune
+
 pushd instructions > /dev/null
 make build
