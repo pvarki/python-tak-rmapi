ce0e7c5645e23c072e6a8787f36193e4c231583c
Ari Karhunen <karppo.ari@gmail.com>
2025-06-05 17:05:38 +0300

    fix: setup tak mgmt and defaults under same lock

--- src/takrmapi/app.py
+++ src/takrmapi/app.py
@@ -1,14 +1,17 @@
 """"factory for the fastpi app"""
 
+import asyncio
+import random
 from typing import AsyncGenerator
 import logging
 from contextlib import asynccontextmanager
+import filelock
 
 from fastapi import FastAPI
 from libpvarki.logging import init_logging
 
 from takrmapi import __version__
-from takrmapi import tak_init
+from takrmapi import tak_init, config
 from .config import LOG_LEVEL
 from .api import all_routers
 
@@ -20,8 +23,21 @@ LOGGER = logging.getLogger(__name__)
 async def app_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     """Handle lifespan management things"""
     # init
-    await tak_init.setup_tak_mgmt_conn()
-    await tak_init.setup_tak_defaults()
+
+    lockpath = config.TAK_CERTS_FOLDER / "takrmapi_init.lock"
+    # Random sleep to the lock file access to avoid race conditions
+    await asyncio.sleep(random.random() / 2)  # nosec
+    lock = filelock.FileLock(lockpath)
+
+    try:
+        lock.acquire(timeout=0.0)
+        await tak_init.setup_tak_mgmt_conn()
+        await tak_init.setup_tak_defaults()
+    except filelock.Timeout:
+        LOGGER.warning("Someone has already locked {}, leaving this init to them".format(lockpath))
+    finally:
+        lock.release()
+
     _ = app
     # App runs
     yield
