a2a3df2aab2d7191d3f678c18a0aa5409f4a6f9d
Eero af Heurlin <eero.afheurlin@iki.fi>
2025-09-07 19:01:47 +0300

    fix: remove the embedded zips, TAK RUNE instrutions need to be modified a lot anyway

--- src/takrmapi/api/instructions.py
+++ src/takrmapi/api/instructions.py
@@ -3,7 +3,6 @@
 from typing import Dict
 import logging
 import json
-import base64
 import asyncio
 from pathlib import Path
 
@@ -50,25 +49,6 @@ async def user_intructions(user: UserCRUDRequest, language: str) -> Dict[str, st
     tak_instructions_data = json.loads(rune_text)
     LOGGER.debug("RUNE JSON loaded")
 
-    LOGGER.debug("Getting zip files")
-    tak_missionpkg = tak_helpers.MissionZip(localuser)
-    zip_base = Path(config.TAK_MISSIONPKG_TEMPLATES_FOLDER)
-    zip_folders = [zip_base / "atak", zip_base / "itak", zip_base / "tak-tracker"]
-    zip_files, tmp_folder = await tak_missionpkg.create_zip_bundles(zip_folders, is_mission_package=True)
-    LOGGER.debug("Got {}".format(zip_files))
-
-    # FIXME: Replace with links to /api/v1/tak-datapackages/clientzip/variant.zip
-    for filestr in zip_files:
-        file = Path(filestr)
-        LOGGER.debug("Embedding {}".format(file))
-        tak_instructions_data.append(
-            {
-                "type": "Asset",
-                "name": file.name,
-                "body": f"data:application/zip;base64,{base64.b64encode(file.read_bytes()).decode('ascii')}",
-            }
-        )
-
     # FIXME: Check the asset type and body for non-embedded assets, this is a best guess
     for pkgpath in Path(config.TAK_MISSIONPKG_TEMPLATES_FOLDER).glob("*"):
         fname = f"{pkgpath.name}.zip"
@@ -77,13 +57,8 @@ async def user_intructions(user: UserCRUDRequest, language: str) -> Dict[str, st
             {
                 "type": "LinkAsset",
                 "name": fname,
-                "body": f'href="{url}"',
+                "body": url,
             }
         )
-
-    LOGGER.debug("Removing {}".format(tmp_folder))
-    # FIXME: Use TaskMaster to background this so we can return earlier
-    await tak_missionpkg.helpers.remove_tmp_dir(str(tmp_folder))
-    LOGGER.debug("Retuning")
 
     return {"callsign": user.callsign, "instructions": json.dumps(tak_instructions_data), "language": language}
