cdae1a3faf5d5e8c759446cc688b3b5786af51db
vitamiini-0 <228652369+vitamiini-0@users.noreply.github.com>
2025-11-20 08:22:06 +0200

    chore: update reference to productdescription model (to libpvarki)

--- src/takrmapi/api/description.py
+++ src/takrmapi/api/description.py
@@ -1,30 +1,13 @@
 """Descriptions API"""
 
-from typing import Optional
 import logging
 
 from fastapi import APIRouter
-from pydantic import BaseModel, Extra, Field  # pylint: disable=(no-name-in-module # false-positive
+from libpvarki.schemas.product import ProductDescription
 
 LOGGER = logging.getLogger(__name__)
 
 router = APIRouter()  # These endpoints are public
-
-
-# FIXME: Move to libpvarki
-class ProductDescription(BaseModel):  # pylint: disable=too-few-public-methods
-    """Description of a product"""
-
-    shortname: str = Field(description="Short name for the product, used as slug/key in dicts and urls")
-    title: str = Field(description="Fancy name for the product")
-    icon: Optional[str] = Field(description="URL for icon")
-    description: str = Field(description="Short-ish description of the product")
-    language: str = Field(description="Language of this response")
-
-    class Config:  # pylint: disable=too-few-public-methods
-        """Pydantic configs"""
-
-        extra = Extra.forbid
 
 
 @router.get(
