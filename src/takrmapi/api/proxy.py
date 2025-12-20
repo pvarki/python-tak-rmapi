207c1303c816c8da80560ff0447c39cc4332a0f9
Your Name <you@example.com>
2025-12-20 01:12:37 +0000

    fix: lint

--- src/takrmapi/api/proxy.py
+++ src/takrmapi/api/proxy.py
@@ -1,3 +1,5 @@
+"""Proxy endpoints for rmapi directed queries"""
+
 from pathlib import Path
 import logging
 
@@ -14,12 +16,9 @@ LOGGER = logging.getLogger(__name__)
 
 router = APIRouter(dependencies=[Depends(MTLSHeader(auto_error=True))])
 
+
 @router.post("/client-zip/{variant}.zip")
-async def return_tak_zip(
-    user: UserCRUDRequest,
-    variant: str,
-    background_tasks: BackgroundTasks
-) -> FileResponse:
+async def return_tak_zip(user: UserCRUDRequest, variant: str, background_tasks: BackgroundTasks) -> FileResponse:
     """Return TAK client zip file"""
 
     localuser = tak_helpers.UserCRUD(user)
@@ -33,20 +32,13 @@ async def return_tak_zip(
 
     tak_missionpkg = TAKPackageZip(localuser)
 
-    target_pkg = TAKDataPackage(
-        template_path=walk_dir, 
-        template_type="mission"
-    )
+    target_pkg = TAKDataPackage(template_path=walk_dir, template_type="mission")
     await tak_missionpkg.create_zip_bundles(datapackages=[target_pkg])
 
     if not target_pkg.zip_path or not target_pkg.zip_path.is_file():
         raise HTTPException(status_code=500, detail="Failed to generate ZIP")
 
-
-    background_tasks.add_task(
-        tak_missionpkg.helpers.remove_tmp_dir, 
-        target_pkg.zip_tmp_folder
-    )
+    background_tasks.add_task(tak_missionpkg.helpers.remove_tmp_dir, target_pkg.zip_tmp_folder)
 
     return FileResponse(
         path=target_pkg.zip_path,
