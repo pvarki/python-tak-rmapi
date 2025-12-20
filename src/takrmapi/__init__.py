c084c93d161f70b806fc5c75e518bf39c32cdf40
Your Name <you@example.com>
2025-12-20 01:13:48 +0000

    chore: bump2version

--- src/takrmapi/__init__.py
+++ src/takrmapi/__init__.py
@@ -1,3 +1,3 @@
 """RASENMAEHER integration API for TAK server"""
 
-__version__ = "1.7.3"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.8.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
