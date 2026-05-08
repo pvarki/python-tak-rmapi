96c0ddaa5dbccafd116850995227a68ad91b97ca
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2026-05-08 11:14:47 +0300

    chore: bump version

--- src/takrmapi/__init__.py
+++ src/takrmapi/__init__.py
@@ -1,3 +1,3 @@
 """RASENMAEHER integration API for TAK server"""
 
-__version__ = "1.12.1+260427"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.13.0+260508"  # NOTE Use `bump-my-version` to bump versions correctly
