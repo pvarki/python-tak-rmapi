4c1585757cf05e0ec6754cd285193425b9c13c84
Eero af Heurlin <eero.afheurlin@iki.fi>
2026-06-12 21:50:18 +0300

    chore: bump version

--- tests/test_takrmapi.py
+++ tests/test_takrmapi.py
@@ -20,7 +20,7 @@ from takrmapi.takutils.tak_pkg_helpers import TAKDataPackage
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.13.0+260508"
+    assert __version__ == "1.13.1+260612"
 
 
 @pytest.fixture(autouse=True)
