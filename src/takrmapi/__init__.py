6195b816219bda1692ff8b907ffafe084e31da32
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2026-07-03 13:34:39 +0300

    chore: bump version

--- src/takrmapi/__init__.py
+++ src/takrmapi/__init__.py
@@ -1,3 +1,3 @@
 """RASENMAEHER integration API for TAK server"""
 
-__version__ = "1.13.1+260612"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.13.2+260703"  # NOTE Use `bump-my-version` to bump versions correctly
--- tests/test_takrmapi.py
+++ tests/test_takrmapi.py
@@ -20,7 +20,7 @@ from takrmapi.takutils.tak_pkg_helpers import TAKDataPackage
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.13.1+260612"
+    assert __version__ == "1.13.2+260703"
 
 
 @pytest.fixture(autouse=True)
