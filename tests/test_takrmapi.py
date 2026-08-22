56d938378a4f6d22a9aeff059cf92d4ba7fbea00
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-08-22 12:12:19 +0300

    chore: bump version to 1.14.2+260822

--- tests/test_takrmapi.py
+++ tests/test_takrmapi.py
@@ -20,7 +20,7 @@ from takrmapi.takutils.tak_pkg_helpers import TAKDataPackage
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.14.1+260822"
+    assert __version__ == "1.14.2+260822"
 
 
 @pytest.fixture(autouse=True)
