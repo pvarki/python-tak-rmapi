99931f49e5e0866e9e84b3f2c3ef13f260708a6a
Eero af Heurlin <rambo@iki.fi>
2026-08-24 22:08:08 +0300

    fix: make sure XML starts at byte 0, app lifetime use the instance created during startup if it was created

--- src/takrmapi/app.py
+++ src/takrmapi/app.py
@@ -31,14 +31,15 @@ async def app_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     await asyncio.sleep(random.random() / 2)  # nosec
     lock = filelock.FileLock(lockpath)
 
+    igniteops_instance: ignite_jni.TAKIgniteOps | None = None
     try:
         lock.acquire(timeout=0.0)
         LOGGER.info("Waiting for REST API")
         await tak_init.wait_for_rest_api()
         # FIXME: Figure out a good way to determine if Ignite is up before calling
         #        it should be safe to assume if REST responds Ignite is up too.
-        LOGGER.info("TAKIgniteOps.singleton()")
-        ignite_jni.TAKIgniteOps.singleton()
+        LOGGER.info("TAKIgniteOps.singleton() from locked tak_init")
+        igniteops_instance = ignite_jni.TAKIgniteOps.singleton()
         LOGGER.info("tak_init.setup_tak_mgmt_conn()")
         await tak_init.setup_tak_mgmt_conn()
         LOGGER.info("tak_init.setup_tak_defaults()")
@@ -53,14 +54,19 @@ async def app_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
         LOGGER.warning("tak_init has not yet completed. Waiting for {} to be relased.".format(lockpath))
         await asyncio.sleep(2)
 
+    if not igniteops_instance:
+        LOGGER.info("TAKIgniteOps.singleton() from other worker after tak_init")
+        igniteops_instance = ignite_jni.TAKIgniteOps.singleton()
+
     await tak_init.get_tak_defaults()
 
     _ = app
     # App runs
     yield
     # Cleanup
-    LOGGER.info("TAKIgniteOps teardown")
-    ignite_jni.TAKIgniteOps.singleton().teardown()
+    if igniteops_instance:
+        LOGGER.info("TAKIgniteOps teardown")
+        igniteops_instance.teardown()
 
 
 def get_app_no_init() -> FastAPI:
